0548e4562d33b1250dd2dd58304c95f0e49af36b
Viyathma Arukgoda <135962654+Viyathmaranlini@users.noreply.github.com>
2025-06-01 13:49:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+    
+} 
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
